39d1a6c84a6baf44634edcdcf9b25bccfa8d45a8
João Pedro Gonçalves <jpedro.fsgs@gmail.com>
2025-01-04 15:05:49 -0300

    first exercise

---
+++ basic-allocation.c
@@ -0,0 +1,39 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main(){
+    int* prt;
+    int n;
+
+    printf("Enter the number of elements: ");
+    scanf("%d", &n);
+
+    prt = (int *) malloc(n * sizeof(int));
+
+    if(prt == NULL){
+        printf("Memory allocation failed\n");
+        exit(1);
+    } else {
+        printf("Memory allocation successful\n");
+
+        for(int i = 0; i < n; i++){
+            int input;
+            printf("Enter the value for element %d: ", i);
+            scanf("%d", &input);
+            prt[i] = input;
+        }
+    }
+
+    printf("Elements: ");
+
+    int i;
+    for(i=0;i<n-1;i++){
+        printf("%d, ", prt[i]);
+    }
+    printf("%d.\n", prt[i]);
+
+    free(prt);
+
+    return 0;
+
+}
